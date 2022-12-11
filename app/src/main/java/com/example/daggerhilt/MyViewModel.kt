1a606dc0feb8a4b7364e99a6d6258211e887fa03
john kenedy <john.kenedy232@gmail.com>
2022-12-11 10:01:59 -0300

    Lazy Injection: Just created when we first use it

--- app/src/main/java/com/example/daggerhilt/MyViewModel.kt
+++ app/src/main/java/com/example/daggerhilt/MyViewModel.kt
@@ -2,10 +2,15 @@ package com.example.daggerhilt
 
 import androidx.lifecycle.ViewModel
 import com.example.daggerhilt.domain.repository.MyRepository
+import dagger.Lazy
 import javax.inject.Inject
 
 
 class MyViewModel @Inject constructor(
-    private val repository: MyRepository
+    private val repository: Lazy<MyRepository>
 ): ViewModel() {
+
+    init {
+        repository.get()
+    }
 }
